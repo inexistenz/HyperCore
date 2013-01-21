1c7cb690815aae680815cccae91bbdf133136753
Jonathan Llewellyn <inexistenz@gmail.com>
2013-01-21 18:51:18 -0500

    Created bullets, added prefab generation functions

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -3,26 +3,44 @@ using System.Collections;
 
 public class MainGame : MonoBehaviour {
 	
-	public GameObject player;
-	public GameObject enemy;
-	public GameObject powerUp;
+	public GameObject playerShip; // Player's Ship Prefab
+	public GameObject enemy; // Enemy Prefab
+	public GameObject powerUp; // Power Up Prefab
 	
-	private float spawnTime = 3;
+	public float gameSpeed = 8; // Speed value that controls all movement in the game
+	
+	
+	private float spawnTime = 3; 
 	private bool spawned = false;
-	private GameObject ship;
+	private GameObject player; // Gameplay reference for player ship
 	// Use this for initialization
 	void Start () {
-		ship = (GameObject) Instantiate (player,Vector3.zero,Quaternion.identity);
+		player = (GameObject) Instantiate (playerShip,Vector3.zero,Quaternion.identity);
+		// Set player speed to gameSpeed
+		PlayerControl playerCtl = player.GetComponent<PlayerControl>();
+		playerCtl.speed = gameSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//spawned = true;
 		if(!spawned && Time.time > spawnTime) {
-			GameObject enemyShip = (GameObject) Instantiate(enemy, new Vector3(10,10,0),Quaternion.identity);
-			Vector3 enemyVelocity = ship.transform.position - enemyShip.transform.position;
-			enemyShip.rigidbody.velocity = enemyVelocity.normalized * 8;
+			generateEnemy(new Vector3(10,10,0));
 			spawned = true;
 		}
+	}
+	
+	// Enemy generation function
+	void generateEnemy(Vector3 pos){
+		GameObject enemyShip = (GameObject) Instantiate(enemy,pos,Quaternion.identity);
+		
+		// Get vector towards player
+		Vector3 enemyVelocity = player.transform.position - enemyShip.transform.position;
+		// Set enemy velocity at 75% of gamespeed
+		enemyShip.rigidbody.velocity = enemyVelocity.normalized * gameSpeed * 0.75f; 
+	}
+	
+	// Power up generation function
+	void generatePowerUp(Vector3 pos){
+		Instantiate(powerUp,pos,Quaternion.identity);
 	}
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
-
-	public float speed = 8; // Base movement speed of the player's ship
+	
+	public GameObject bullet; // Bullet prefab
+	public float speed; // Base movement speed of the player's ship
 	
 	private Vector3 vertical = new Vector3(0,1,0); // Unit y vector for creating ship velocity 
 	private Vector3 horizontal = new Vector3(1,0,0); // Unit x vector for creating ship velocity
@@ -16,9 +17,13 @@ public class PlayerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// Make ship velocity based on input
-		// TODO: use sin/cos to make max velocity alway equal speed variable
+		// TODO: use sin/cos to make max velocity always equal speed variable
 		rigidbody.velocity = vertical * speed * Input.GetAxis("Vertical")
 			+ horizontal * speed * Input.GetAxis("Horizontal");
+		
+		if(Input.GetButtonUp("Fire1")){
+			fireBullet();
+		}
 	}
 	
 	void OnTriggerEnter(Collider collider) {
@@ -27,6 +32,15 @@ public class PlayerControl : MonoBehaviour {
 		Debug.Log(this.name + " collided with: " + collider.transform.name);
 	}
 	
+	// Bullet generation function
+	void fireBullet() {
+		GameObject shot = (GameObject) Instantiate(bullet,transform.position,Quaternion.identity);
+		float angle;
+		Vector3 axis;
+		transform.rotation.ToAngleAxis(out angle, out axis); // Get ship orientation
+		shot.rigidbody.velocity = axis * speed * 1.5f; // Fire bullet along that axis at 1.5x gamespeed
+	}
+
 	/*public void Hit() {
 		Debug.Log ("Enemy Contact");
 	}*/
